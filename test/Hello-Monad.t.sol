a48a62050e9b676ea61ff4fd02225d23eb4d46c0
dan-bosy <ai.node.network@gmail.com>
2025-06-17 19:51:42 +0100

    Create Hello-Monad.t.sol

---
+++ test/Hello-Monad.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import "forge-std/Test.sol";
+import "../src/HelloMonad.sol";
+
+contract HelloTest is Test {
+    function testGreet() public {
+        HelloMonad h = new HelloMonad();
+        assertEq(h.greet(), "Hello, Monad!");
+    }
